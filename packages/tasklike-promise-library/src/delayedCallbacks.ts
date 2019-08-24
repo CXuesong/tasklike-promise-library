dc70fa88e9cd16f7e9396980d5f6c180a76f5b99
forest93 <forest93@163.com>
2019-08-24 14:59:26 +0800

    Add delayedCallbacks for context switching.

---
+++ packages/tasklike-promise-library/src/delayedCallbacks.ts
@@ -0,0 +1,127 @@
+/**
+ * @module
+ * Contains adapter functions that allows you to switch into another context when you subscribe the returned `PromiseLike`s.
+ * For example, in async functions, if you want to execute part of your logic inside an
+ * [animation frame](https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame),
+ * you may now use
+ * ```ts
+ * async function playAnimation(cancellationToken?: ICancellationToken): Promise<void> {
+ *     cancellationToken && cancellationToken.throwIfCancellationRequested();
+ *     while (true) {
+ *         const animationFrame = await requestAnimationFrameAsync(cancellationToken);
+ *         // TBC.
+ *     }
+ * }
+ * ```
+ */
+import { IConfigurablePromiseLike, IDisposable, PromiseLikeResolutionSource } from "./primitives";
+import { ICancellationToken } from "./primitives/cancellation";
+
+/**
+ * Gets a `PromiseLike` that resolves inside `window.setTimeout` callback.
+ * @param milliseconds the delay in milliseconds before the `PromiseLike` resolves.
+ * @param cancellationToken a token used to cancel the timeout with `window.clearTimeout`.
+ * @returns a `PromiseLike` that resolves inside `window.setTimeout` callback.
+ * Resolved value is the timeout ID returned from `window.setTimeout`.
+ */
+export function setTimeoutAsync(milliseconds: number, cancellationToken?: ICancellationToken): IConfigurablePromiseLike<number> {
+    cancellationToken && cancellationToken.throwIfCancellationRequested();
+    const plrs = new PromiseLikeResolutionSource<number>();
+    let subscription: undefined | IDisposable;
+    const id = window.setTimeout(() => {
+        subscription && subscription.dispose();
+        plrs.tryResolve(id);
+    }, milliseconds);
+    if (cancellationToken) {
+        subscription = cancellationToken.subscribe(() => {
+            window.clearTimeout(id);
+            plrs.tryCancel();
+        });
+    }
+    return plrs.promiseLike;
+}
+
+/**
+ * Provides context for {@link requestAnimationFrameAsync}.
+ */
+export interface IRequestAnimationFrameContext {
+    /** RAF ID. */
+    id: number;
+    /** Time when the RAF triggered. */
+    time: number;
+}
+
+/**
+ * Gets a `PromiseLike` that resolves inside `window.requestAnimationFrame` callback.
+ * @param cancellationToken a token used to cancel the timeout with `window.cancelAnimationFrame`.
+ * @returns a `PromiseLike` that resolves inside `window.requestAnimationFrame` callback.
+ * Resolved value contains RAF context.
+ */
+export function requestAnimationFrameAsync(cancellationToken?: ICancellationToken): IConfigurablePromiseLike<Readonly<IRequestAnimationFrameContext>> {
+    cancellationToken && cancellationToken.throwIfCancellationRequested();
+    const plrs = new PromiseLikeResolutionSource<IRequestAnimationFrameContext>();
+    let subscription: undefined | IDisposable;
+    const id = window.requestAnimationFrame((time) => {
+        subscription && subscription.dispose();
+        plrs.tryResolve({ id, time });
+    });
+    if (cancellationToken) {
+        subscription = cancellationToken.subscribe(() => {
+            window.cancelAnimationFrame(id);
+            plrs.tryCancel();
+        });
+    }
+    return plrs.promiseLike;
+}
+
+export interface IRequestIdleCallbackOptions {
+    timeout?: number;
+}
+
+export interface IRequestIdleCallbackDeadline {
+    didTimeout: boolean;
+    timeRemaining(): number;
+}
+
+/**
+ * Provides context for {@link requestAnimationFrameAsync}.
+ */
+export interface IRequestIdleCallbackContext {
+    /** RequestIdleCallback ID. */
+    id: number;
+    /** Deadline information. */
+    deadline: IRequestIdleCallbackDeadline;
+}
+
+interface IWindowExp {
+    requestIdleCallback(callback: (deadline: IRequestIdleCallbackDeadline) => void, options?: IRequestIdleCallbackOptions): number;
+    cancelIdleCallback(handle: number): void;
+}
+
+/**
+ * @experimental
+ * Gets a `PromiseLike` that resolves inside `window.requestIdleCallback` callback.
+ * @param options options passed into `requestIdleCallback` function.
+ * @param cancellationToken a token used to cancel the timeout with `window.cancelIdleCallback`.
+ * @returns a `PromiseLike` that resolves inside `window.requestIdleCallback` callback.
+ * Resolved value contains requestIdleCallback context.
+ */
+export function requestIdleCallbackAsync(
+    options?: IRequestIdleCallbackOptions,
+    cancellationToken?: ICancellationToken
+): IConfigurablePromiseLike<Readonly<IRequestIdleCallbackContext>> {
+    cancellationToken && cancellationToken.throwIfCancellationRequested();
+    const plrs = new PromiseLikeResolutionSource<IRequestIdleCallbackContext>();
+    let subscription: undefined | IDisposable;
+    const id = (window as unknown as IWindowExp).requestIdleCallback((deadline) => {
+        subscription && subscription.dispose();
+        plrs.tryResolve({ id, deadline });
+    }, options);
+    if (cancellationToken) {
+        subscription = cancellationToken.subscribe(() => {
+            (window as unknown as IWindowExp).cancelIdleCallback(id);
+            plrs.tryCancel();
+        });
+    }
+    return plrs.promiseLike;
+}
